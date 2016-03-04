c98df6e1653fd337073da94e5ff788a4e069e847
lukee910 <lukas.doerig@zeitag.ch>
2016-03-04 11:02:24 +0100

    M105

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {M307Component} from './components/m307/m307';
+import {M105Component} from './components/m105/m105';
 import {HomeComponent} from './components/home/home';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
@@ -17,6 +18,10 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
     path: '/m307',
     name: 'M307',
     component: M307Component
+}, {
+    path: '/m105',
+    name: 'M105',
+    component: M105Component
 }, {
     path: '/',
     name: 'Home',
---
+++ app/components/m105/m105.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+import {Route} from '../../model/Route';
+
+@Component({
+    templateUrl: 'app/components/m105/m105.html'
+})
+export class M105Component {}
