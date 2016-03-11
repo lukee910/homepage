a2fcdf56cf5f77de1efff900962b25c20b0480e4
lukee910 <lukas.doerig@zeitag.ch>
2016-03-11 10:50:26 +0100

    Some fancy things

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,10 +1,26 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 @Component({
     templateUrl: 'app/home/home.html',
     directives: [
         ROUTER_DIRECTIVES
+    ],
+    viewProviders: [        
+        HTTP_PROVIDERS
     ]
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+    private issuesReportHref = '';
+    
+    constructor(private http: Http) {}
+    
+    ngOnInit() {
+        var setHref = (response) => {
+            this.issuesReportHref = response.json().bugs.url;
+        };
+        this.http.get('/package.json')
+            .subscribe(setHref);
+    }
+ }
