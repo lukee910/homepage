a9c894fa91603570f9d83890ab6dc8671a62a3c7
Lukas Dörig <lukee910@gmail.com>
2016-06-24 10:34:31 +0200

    Use HashLocationStrategy

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,10 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {provide} from "angular2/core";
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    provide(APP_BASE_HREF, {useValue: '/#'}),
+    provide(LocationStrategy, {useClass: HashLocationStrategy})
+]);
